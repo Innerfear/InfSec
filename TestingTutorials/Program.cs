256259a14ff7d880d1b75f57bd09602e2515ced1
Innerfear <ziggy.streulens@student.ap.be>
2015-11-20 14:43:34 +0100

    Get Device List
    Tutorial stap 1: device list verkrijgen

---
+++ TestingTutorials/Program.cs
@@ -0,0 +1,39 @@
+﻿using SharpPcap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SharpPcap version
+            string ver = SharpPcap.Version.VersionString;
+            Console.WriteLine("SharpPcap{0}, Example1.IfList.cs", ver );
+
+            //Device list verkrijgen
+            CaptureDeviceList devices = CaptureDeviceList.Instance;
+
+            //Error bij geen devices
+            if (devices.Count < 1)
+            {
+                Console.WriteLine("No devices were found on this machine.");
+                return;
+            }
+
+            Console.WriteLine("\nThe following devices are available on this machine:");
+            Console.WriteLine("----------------------------------------------------\n");
+
+            //Print gevonden network devices
+            foreach(ICaptureDevice dev in devices)
+                Console.WriteLine("{0}\n", dev.ToString());
+
+            Console.WriteLine("Hit 'Enter' to exit...");
+            Console.ReadLine();
+        }
+    }
+}
