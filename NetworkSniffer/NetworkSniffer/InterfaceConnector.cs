0ca4a36c1667a95f743eedaae3304b9661e24653
Viraptorus <Jasper.van.gestel@hotmail.com>
2015-12-03 17:55:21 +0100

    Testing New Library
    eExNetwork library tests:
    - HandlerTemplate

---
+++ NetworkSniffer/NetworkSniffer/InterfaceConnector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using eExNetworkLibrary;
+using eExNetworkLibrary.Utilities;
+
+namespace NetworkSniffer
+{
+    class InterfaceConnector : TrafficHandler
+    {
+        WinPcapInterface[] arWpc = EthernetInterface.GetAllPcapInterfaces();
+
+        public override void Cleanup()
+        {
+            throw new NotImplementedException();
+        }
+
+        // This method is called when the traffic handler is removed from 
+        // a compilation and should stop down.
+        // This method should in first place cleanup memory and stop threads, but
+        // do no time-consuming recovery operations which involve the network.
+        public override void Stop()
+        {
+            //Place stop stuff here.
+            base.Stop(); //Don't forget to stop the engines
+        }
+
+        // This method is called when the traffic handler is inserted into a
+        // network compilation and should start up
+        public override void Start()
+        {
+            //Place start stuff here.
+            base.Start(); //Don't forget to start the engines
+        }
+
+        protected override void HandleTraffic(Frame fInputFrame)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetworkSniffer/NetworkSniffer/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using eExNetworkLibrary;
+using System.Windows;
+
+namespace NetworkSniffer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainHandler HandlerTemp = new MainHandler();
+        InterfaceConnector IntConn = new InterfaceConnector();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void StartMethod()
+        {
+            HandlerTemp.Start();
+        }
+
+        private void RandomMethod()
+        {
+
+        }
+        
+    }
+}
