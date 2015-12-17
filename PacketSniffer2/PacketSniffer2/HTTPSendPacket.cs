b9c472f5e9c0c7f08b0c9616680db3c760ecefc2
Innerfear <ziggy.streulens@student.ap.be>
2015-12-17 10:35:38 +0100

    Revert "Update HTTP"
    This reverts commit a4313a2d77cd42f2970719ced9e8edf2524e1943.

--- PacketSniffer2/PacketSniffer2/HTTPSendPacket.cs
+++ PacketSniffer2/PacketSniffer2/HTTPSendPacket.cs
@@ -1,8 +1,5 @@
 ﻿using PcapDotNet.Packets;
 using PcapDotNet.Packets.Ethernet;
-using PcapDotNet.Packets.Http;
-using PcapDotNet.Packets.IpV4;
-using PcapDotNet.Packets.Transport;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,50 +14,11 @@ namespace PacketSniffer2
     class HTTPSendPacket : BaseSendPacket
     {
         public Packet HTTPpacket;
-        protected IpV4Layer ipv4Layer;
-        protected TcpLayer tcpLayer;
-        protected HttpRequestLayer httpLayer;
-        public HTTPSendPacket(string MACsrc, string MACdst, string IPsrc, string IPdst, string IpId, string TTL, string PORTsrc, string PORTdst, string SQN, string ACK, string WIN, string data, string Domain)
+        public HTTPSendPacket(string MACsrc, string MACdst)
         {
             GetBase(MACsrc, MACdst);
 
-            ipv4Layer =
-            new IpV4Layer
-            {
-                Source = new IpV4Address(IPsrc),
-                CurrentDestination = new IpV4Address(IPdst),
-                Fragmentation = IpV4Fragmentation.None,
-                HeaderChecksum = null, // Will be filled automatically.
-                Identification = StringToUShort(IpId),
-                Options = IpV4Options.None,
-                Protocol = null, // Will be filled automatically.
-                Ttl = StringToByte(TTL),
-                TypeOfService = 0,
-            };
-
-            tcpLayer =
-            new TcpLayer
-            {
-                SourcePort = StringToUShort(PORTsrc),
-                DestinationPort = StringToUShort(PORTdst),
-                Checksum = null, // Will be filled automatically.
-                SequenceNumber = StringToUShort(SQN),
-                AcknowledgmentNumber = StringToUShort(ACK),
-                ControlBits = TcpControlBits.Acknowledgment,
-                Window = StringToUShort(WIN),
-                UrgentPointer = 0,
-                Options = TcpOptions.None,
-            };
-
-            httpLayer =
-            new HttpRequestLayer
-            {
-                Version = HttpVersion.Version11,
-                Header = new HttpHeader(new HttpContentLengthField(11)),
-                Body = new Datagram(Encoding.ASCII.GetBytes(data)),
-                Method = new HttpRequestMethod(HttpRequestKnownMethod.Get),
-                Uri = Domain,
-            };
+            //CODE HIER
         }
         public override void GetBase(string MACsrc, string MACdst)
         {
@@ -69,9 +27,6 @@ namespace PacketSniffer2
         public void GetBuilder()
         {
             listLayers.Add(ethernetLayer);
-            listLayers.Add(ipv4Layer);
-            listLayers.Add(tcpLayer);
-            listLayers.Add(httpLayer);
             AddLayers(listLayers);
             HTTPpacket = builder.Build(DateTime.Now);
         }
